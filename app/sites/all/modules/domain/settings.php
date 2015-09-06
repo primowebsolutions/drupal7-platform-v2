30d02575b1874ac352f1f3d677c322cedde9b25f
primowebsolutions <geraldk@primowebsolutions.com>
2015-09-05 22:18:40 -0700

    Added app

---
+++ app/sites/all/modules/domain/settings.php
@@ -0,0 +1,4 @@
+/**
+ * Add the domain module setup routine.
+ */
+include DRUPAL_ROOT . '/sites/all/modules/domain/settings.inc';
---
+++ permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod -R 550 /app
+chmod -R 775 /app/sites/default
+chmod 440 /app/sites/default/settings.php
